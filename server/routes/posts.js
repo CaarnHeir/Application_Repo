3488248fd0fc2643f26652a7f5c3a21333a836ad
Caarn Heir <caarnheir@gmail.com>
2022-01-04 22:45:41 -0500

    built out the get and post functionality for posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,9 +2,10 @@
 
 import express, { response } from 'express';
 
-import { getPosts } from '../controllers/posts.js';
+import { getPosts, createPost } from '../controllers/posts.js';
 const router = express.Router();
 
 router.get('/', getPosts);
+router.post('/', createPost);
 
 export default router;
