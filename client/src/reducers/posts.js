2167e4650ff755520f4ce92e6f0badb0e0652dcd
Caarn Heir <caarnheir@gmail.com>
2022-01-19 22:07:12 -0500

    comment functionality added

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,10 +1,10 @@
-import { FETCH_ALL, FETCH_BY_SEARCH, FETCH_POST, CREATE, UPDATE, DELETE, INTERACTION } from '../constants/actionTypes';
+import { FETCH_ALL, FETCH_BY_SEARCH, FETCH_POST, CREATE, UPDATE, DELETE, INTERACTION, COMMENT,START_LOADING,END_LOADING } from '../constants/actionTypes';
 
 export default (state = { isLoading: true, posts: [] }, action) => {
   switch (action.type) {
-    case 'START_LOADING':
+    case START_LOADING:
       return { ...state, isLoading: true };
-    case 'END_LOADING':
+    case END_LOADING:
       return { ...state, isLoading: false };
     case FETCH_ALL:
       return {
@@ -19,6 +19,14 @@ export default (state = { isLoading: true, posts: [] }, action) => {
       return { ...state, post: action.payload.post };
     case INTERACTION:
       return { ...state, posts: state.posts.map((post) => (post._id === action.payload._id ? action.payload : post)) };
+    case COMMENT:
+      return { ...state, posts: state.posts.map((post) => {
+        if(post._id === action.payload._id) {
+          return action.payload;
+        }
+        return post;
+      })
+    };
     case CREATE:
       return { ...state, posts: [...state.posts, action.payload] };
     case UPDATE:
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,7 @@
 //Handling all routing for posts
 import express from 'express';
 // import express, { response } from 'express';
-import { getPosts, createPost, updatePost, deletePost, interactionPost, getPost, commentPost, getPostsBySearch } from '../controllers/posts.js';
+import { getPosts, createPost, updatePost, deletePost, getPost, commentPost, getPostsBySearch } from '../controllers/posts.js';
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
@@ -13,6 +13,7 @@ router.get('/:id', getPost);
 router.post('/', auth,  createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
+router.post('/:id/commentPost',auth, commentPost);
 // router.patch('/:id/likePost', auth, likePost);
 // router.post('/:id/commentPost', commentPost);
 
