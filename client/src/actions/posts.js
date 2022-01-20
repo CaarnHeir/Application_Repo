413234b23e70249caa5bfca40045530a8ec25688
Caarn Heir <caarnheir@gmail.com>
2022-01-19 21:16:08 -0500

    minor fixes, routing on create post now directly to post details page

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -8,7 +8,6 @@ export const getPosts = (page) => async (dispatch) => {
   try {
     dispatch({ type: START_LOADING });
     const { data: { data, currentPage, numberOfPages } } = await api.fetchPosts(page);
-
     dispatch({ type: FETCH_ALL, payload: { data, currentPage, numberOfPages } });
     dispatch({ type: END_LOADING });
   } catch (error) {
@@ -16,11 +15,11 @@ export const getPosts = (page) => async (dispatch) => {
   }
 };
 
-export const createPost = (post) => async (dispatch) => {
+export const createPost = (post, history) => async (dispatch) => {
     try {
         dispatch({ type: START_LOADING });
         const { data } = await api.createPost(post);
-        
+        history.push(`/posts/${data._id}`);
         dispatch({ type: CREATE, payload: data});
     } catch (error) {
         console.log(error);
