53e4e2681067d0274687b57b2f76a15397df4d5d
Caarn Heir <caarnheir@gmail.com>
2022-01-23 19:57:03 -0500

    resolved sign in refresh issue, no longer need to manually refresh page to access /auth via signup button in Navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -30,6 +30,11 @@ export const Navbar = () => {
     
         setUser(JSON.parse(localStorage.getItem('profile')));
       }, [location]);
+
+      const handleClick = () => {
+          logout();
+          window.location.reload();
+      };
   
     return (
         <AppBar className = { classes.appBar } position = 'static' color = 'inherit'>
@@ -42,7 +47,7 @@ export const Navbar = () => {
                     <div className = { classes.profile }>
                         <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
                         <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
-                        <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+                        <Button variant="contained" className={classes.logout} color="secondary" onClick={handleClick}>Logout</Button>
                     </div>
                 ):(
                     <Button component = { Link } to = '/auth' variant = 'contained' color = 'primary'> Sign In </Button>
